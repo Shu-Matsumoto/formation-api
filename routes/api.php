7895a7008f02974cc4c52e7caf268b511c37af09
Shu-Matsumoto <103585072+Shu-Matsumoto@users.noreply.github.com>
2022-09-17 00:25:36 +0900

    各種APIコントローラ追加(中身空)

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,15 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\SkillController;
+use App\Http\Controllers\Api\LectureController;
+use App\Http\Controllers\Api\StudentController;
+use App\Http\Controllers\Api\TeacherController;
+use App\Http\Controllers\Api\LectureScheduleController;
+use App\Http\Controllers\Api\TeachingMaterialController;
+use App\Http\Controllers\Api\ApplicationOfLectureController;
+use App\Http\Controllers\Api\TeamChatController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +26,16 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// DBテーブル各々に対応したルーティング
+// Route::resource([
+//     'users' => UserController::class,
+//     'skills' => SkillController::class,
+//     'lectures' => LectureController::class,
+//     'students' => StudentController::class,
+//     'teachers' => TeacherController::class,
+//     'lecture_schedules' => LectureScheduleController::class,
+//     'teaching_materials' => TeachingMaterialController::class,
+//     'application_of_lectures' => ApplicationOfLectureController::class,
+//     'team_chats' => TeamChatController::class,
+// ]);
